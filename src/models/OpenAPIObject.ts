6958e4b64ae63427d7c6db96b564a1949a3eb3ba
AJ Tech City <arunjai12456@icloud.com>
2026-07-31 19:06:26 +0100

    OpenAPIObject Basics done

--- src/models/OpenAPIObject.ts
+++ src/models/OpenAPIObject.ts
@@ -1,17 +1,15 @@
 import { InfoObject } from "./InfoObject";
+import { PathsObject } from "./PathsObject";
+import { ServerObject } from "./ServerObject";
 
-export interface OpenAPIObject{
-    openapi: string; //Must be the version number of the OpenAPI Spec the document uses
-    $self: string; //Self-assigned URI of the document
-    info: InfoObject; //REQUIRED: Provides metadata about the API
-    jsonSchemaDialect: string; //default value for the $schema keyword within SchemaObjects. Must be a URI
-}
+// export interface OpenAPIObject{
+//     openapi: string; //Must be the version number of the OpenAPI Spec the document uses
+//     $self: string; //Self-assigned URI of the document
+//     info: InfoObject; //REQUIRED: Provides metadata about the API
+//     jsonSchemaDialect: string; //default value for the $schema keyword within SchemaObjects. Must be a URI
+// }
 
 export class OpenAPIObject{
-    openapi: string;
-    $self: string;
-    info: InfoObject;
-    jsonSchemaDialect: string;
 
     public static readonly example: OpenAPIObject = new OpenAPIObject(
         "3.1.0",
@@ -21,14 +19,10 @@ export class OpenAPIObject{
     )
 
     constructor(
-        openapi: string,
-        $self: string,
-        info: InfoObject,
-        jsonSchemaDialect: string
-    ){
-        this.openapi = openapi;
-        this.$self = $self;
-        this.info = info;
-        this.jsonSchemaDialect = jsonSchemaDialect
-    }
+        public openapi: string,
+        public info: InfoObject,
+        public jsonSchemaDialect?: string,
+        public servers?: ServerObject[],
+        public paths?: PathsObject
+    ){}
 }
